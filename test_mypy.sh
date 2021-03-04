da97d7e9edfe64c9353d03ce6c99ef0f2c1e5fb4
apple <apple@apples-Mac.local>
2021-03-03 16:13:01 -0800

    Enable type checking

---
+++ test_mypy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+serverIp=159.89.154.87
+git clone https://github.com/snsnlou2/mypy.git
+cd mypy
+git checkout release-0.800
+git submodule init
+git submodule update
+cd ..
+pip install -U ./mypy
+rm -rf ./mypy
+python3 typecheck.py
+eval "$(ssh-agent -s)"
+chmod 600 root_key
+ssh-keyscan $serverIp >> ~/.ssh/known_hosts
+ssh-add root_key
+zip -r mypy_test_cache.zip mypy_test_cache/
+owner=flairNLP
+repo=flair-Pair192-after
+pv=$(python3 -V | cut -c8-10)
+arc=$(uname -m)
+yes | scp -i root_key ./mypy_test_cache.zip "root@$serverIp:~/cache/$owner---$repo\($pv---$arc\).zip"
